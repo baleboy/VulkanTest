0f8baf886df306dd948419b9ee3993143d51d6bb
Francesco Balestrieri <bale@balenet.com>
2020-12-30 11:48:49 +0200

    Add function to select the best surface format.

--- VulkanTest/Source.cpp
+++ VulkanTest/Source.cpp
@@ -283,6 +283,16 @@ private:
         return details;
     }
 
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR> formats) {
+        for (const auto& format : formats) {
+            if (format.format == VK_FORMAT_B8G8R8A8_SRGB &&
+                format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+                return format;
+            }
+        }
+        return formats[0];
+    }
+
     void createLogicalDevice() {
         QueueFamilyIndices indices = findQueueFamilies(m_physicalDevice);
 
